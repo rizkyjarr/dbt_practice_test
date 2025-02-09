974c6379453620de8f4e9b8048ac392962412cee
Rizky Fajar <rizkyjarr@gmail.com>
2025-02-09 10:59:18 +0700

    successfully developed dim_driver, proceeds to create fact rides

--- dbt_project/models/facts/dim_driver.sql
+++ dbt_project/models/facts/dim_driver.sql
@@ -10,18 +10,35 @@
 }}
 
 
-WITH source AS (
+WITH driver AS (
     SELECT *
-    FROM {{ source('staging_tables', 'customer_data') }}
+    FROM {{ source('staging_tables', 'driver_data') }}
+
 ),
 
-cleaned AS (
-    SELECT
-        cust_id,
-        name,
-        -- Standardizing phone numbers (removing parentheses, dashes, spaces)
-        REGEXP_REPLACE(phone_number, r'[\s\-\(\)]', '') AS phone_number,
-        email,
-        created_at
-    FROM source
+vehicle AS (
+    SELECT *
+    FROM {{ source('staging_tables', 'vehicle_data')}}
 )
+
+SELECT
+    driver.driver_id,
+    vehicle.vehicle_id,
+    driver.name as driver_name,
+    driver.phone_number as phone_number,
+    driver.email as email,
+    vehicle.vehicle_type,
+    vehicle.brand as vehicle_brand,
+    vehicle.year as vehicle_production_year,
+    vehicle.license_plate,
+    driver.created_at
+FROM driver
+LEFT JOIN vehicle
+on driver.driver_id = vehicle.vehicle_id
+
+{% if check_if_incremental() %}
+    WHERE created_at > (
+        SELECT MAX(created_at)
+        FROM {{ this }}
+    )
+{% endif %}
